76737d8b60242f966bbc045fba17d4c49a7287ff
Nick <inikolay.axenov@gmail.com>
2015-07-10 21:25:25 +0300

    create button

--- testingProject/ViewController.swift
+++ testingProject/ViewController.swift
@@ -12,14 +12,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var button = UIButton.buttonWithType(UIButtonType.System) as! UIButton
+        button.frame = CGRectMake(10, 10 , 100, 100)
+        button.setTitle("Button", forState: UIControlState.Normal)
+        button.addTarget(self, action: "Action:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(button)
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func Action(sender: UIButton){
+        UIView.animateWithDuration(1, animations: {
+            
+            self.view.backgroundColor = UIColor.cyanColor()
+            
+        })
+        
     }
-
-
+    
+    
+    
+    
+    
+    
 }
 
